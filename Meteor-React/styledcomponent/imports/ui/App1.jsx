a02568de2b33db96946d15fa5046b39a85e00486
M3L-DC <melanie.da-costa@u-psud.fr>
2019-02-07 12:30:42 +0100

    4th commit : begining of styled component + begining of css

--- Meteor-React/styledcomponent/imports/ui/App1.jsx
+++ Meteor-React/styledcomponent/imports/ui/App1.jsx
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
-const App1 = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-  </div>
-);
+const AppWrapper = styled.section`
+  text-align: center;
+  height: 100vh;
+`
+const AppHeader = styled.header`
+  background-color: #222;
+  height: auto;
+  padding: 20px;
+  color: white;
+`
+const AppDiv = styled.div`
+  background-color: #FFF;
+  border: 2px solid #222;
+  height: 150px;
+  padding: 20px;
+  justify-content: center;
+ `
+const AppTitle = styled.h1`
+  font-size: 1.3em;
+`
+const AppIntro = styled.p`
+  font-size: large;
+`
+const AppInput = styled.input`
+  font-size: large;
+  
+`
+const AppButton = styled.button`
+  font-size: large;
+  margin: 0px 10px;
+  padding: 5px;
+  background-color: #000;
+  color: #FFF;
+  border-style: none;
+  border-radius: 5px;
+`
+
+class App1 extends Component {
+  state = {
+    login : "",
+    password : ""
+  }
+
+  onChange = e => {
+    this.state(
+      { [ e.target.name ] : e.target.value }
+    );
+  }
+
+  onClick = e => {
+    console.log( this.state.login, this.state.password );
+  }
+
+  render() {
+    return(
+      <AppWrapper>
+        <AppHeader>
+          <AppTitle>Welcome to Marvellous app!</AppTitle>
+        </AppHeader>
+
+        <AppDiv>
+          <AppInput 
+            label="Login"
+            name="login"
+            onChange={ this.onChange }
+            placeholder="Votre login"
+          />
+
+          <AppInput 
+            label="Login"
+            name="login"
+            onChange={ this.onChange }
+            placeholder="Votre login"
+          />
+
+          <AppButton onClick={ this.onClick }>S'authentifier</AppButton>
+        </AppDiv>
+      </AppWrapper>
+    )
+  }
+}
 
 export default App1;
---
+++ README2.md
@@ -0,0 +1,36 @@
+
+CSS & Styled-component
+======================
+
+First step : Installation
+------------
+To install styled-component, it only takes one single line :
+
+npm install --save styled-components
+or
+npm i styled-components
+
+
+*********
+The Beginning
+---------
+###First Exemple
+###Adapting Based On Props
+###Extending Styles
+###Styling Every Component
+
+How Does It Work ?
+------------------
+###
+
+
+Source :
+    * https://www.styled-components.com/docs/basics
+    * https://www.styled-components.com/docs
+
+echo "# Documentation_styled-component" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/M3L-DC/Documentation_styled-component.git
+git push -u origin master
